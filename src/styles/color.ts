f99cff42d918f7d8c8805caca4328632859c9220
Resister-boy <hassanpumped17@gmail.com>
2023-10-06 15:21:53 +0900

    feat: Initial Settings

---
+++ src/styles/color.ts
@@ -0,0 +1,33 @@
+export type ColorMap = {
+  [theme: string]: string;
+}
+
+export const lightColor = {
+  '--primary-1': '#0047FF',
+  '--primary-2': '#336CFF',
+  '--primary-3': '#6590FF',
+  '--primary-4': '#EDEFFF',
+  '--white': '#ffffff',
+  '--dark': '#000000',
+  '--background-1': '#F2F2F2',
+  '--background-2': '#E7E7E7',
+  '--gray-1': '#D3D4D8',
+  '--gray-2': '#ABACB2',
+  '--gray-3': '#747579',
+  '--gray-4': '#313234'
+}
+
+export const darkColor = {
+  '--primary-1': '#0047FF',
+  '--primary-2': '#336CFF',
+  '--primary-3': '#6590FF',
+  '--primary-4': '#EDEFFF',
+  '--white': '#ffffff',
+  '--dark': '#000000',
+  '--background-1': '#1A1A1A',
+  '--background-2': '#222326',
+  '--gray-1': '#333439',
+  '--gray-2': '#515358',
+  '--gray-3': '#797A81',
+  '--gray-4': '#cccccc'
+}
