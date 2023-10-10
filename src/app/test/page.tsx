b93ec7a176e52f0c1e12a18b7da5cf39070ff6f2
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-10-11 08:26:11 +0900

    Feat: Payment Method modal

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,21 @@
 'use client';
 
-import BasicTag from '@/components/base/tag/BasicTag';
+import BasicModal from '@/components/base/modal/BasicModal';
+import PaymentMethodModal from '@/components/base/modal/PaymentMethodModal';
 import BeforeRegisterChallenge from '@/components/common/beforeRegisterChallenge/BeforeRegisterChallenge';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Test = () => {
+  const [isPaymentMethodModalOpen, setIsPaymentMethodModalOpen] =
+    useState(true);
   return (
     <Container>
       <BeforeRegisterChallenge />
+      <PaymentMethodModal
+        isModalOpen={true} //이렇게 해야만 열림... 왜..?
+        setIsModalOpen={setIsPaymentMethodModalOpen}
+      />
     </Container>
   );
 };
--- src/components/base/button/BasicButton.tsx
+++ src/components/base/button/BasicButton.tsx
@@ -1,40 +1,51 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import { ButtonProp } from '@/types/Button';
 
 type BasicButtonT = {
   color: string;
   fontSize: number;
-}
+  borderRadius: number;
+  backgroundColor: string;
+};
 
-
-const BasicButton = ({ title, onClickHandler, color, fontSize }: BasicButtonT & ButtonProp) => {
-  
-  const BasicButton = styled.button<BasicButtonT>`
-    width: 100%;
-    min-width: 50px;    
-    height: 100%;
-    min-height: 20px;
-    font-size: ${(props) => `${(props.fontSize)}px`};
-    color: ${(props) => props.color};
-    text-align: center;
-    border: none;
-    transition: all ease .2s;
-
-    &:hover {
-      cursor: pointer;
-    }
-  `
-
+const BasicButton = ({
+  title,
+  onClickHandler,
+  color,
+  fontSize,
+  borderRadius,
+  backgroundColor,
+}: BasicButtonT & ButtonProp) => {
   return (
-    <BasicButton 
-      color={color} 
+    <BasicButtonWrapper
+      color={color}
       fontSize={fontSize}
       onClick={() => onClickHandler()}
+      borderRadius={borderRadius}
+      backgroundColor={backgroundColor}
     >
       {title}
-    </BasicButton>
-  )
-}
+    </BasicButtonWrapper>
+  );
+};
 
-export default BasicButton
+export default BasicButton;
+
+const BasicButtonWrapper = styled.button<BasicButtonT>`
+  width: 100%;
+  min-width: 50px;
+  height: 100%;
+  min-height: 20px;
+  font-size: ${(props) => `${props.fontSize}px`};
+  color: ${(props) => props.color};
+  text-align: center;
+  border: none;
+  transition: all ease 0.2s;
+  border-radius: ${(props) => `${props.borderRadius}px`};
+  background-color: ${(props) => props.backgroundColor};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
