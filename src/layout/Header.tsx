8756f6862922dd2cd3206fd5624a838c5cad4f99
Resister-boy <hassanpumped17@gmail.com>
2023-10-10 18:00:12 +0900

    feat: update layout rendering condition

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,6 @@
 import AuthSet from '@/components/common/authSet/AuthSet'
-import { useGetTitle } from '@/hook/useGetTitle'
-import React from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const HeaderContainer = styled.header`
@@ -8,6 +8,54 @@ const HeaderContainer = styled.header`
   height: auto;
   display: flex;
   flex-direction: column;
+  `
+
+const NavItem = styled.div<{display: string, animation: string}>`
+  position: relative;
+  width: 50%;
+  min-width: 160px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid #6F7789;
+  
+  &:hover {
+    cursor: pointer;
+  }
+  
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -2px;
+    width: 100%;
+    border: 2px solid #000000;
+    border-radius: 2px;
+    z-index: 9;
+    display: ${(props) => props.display};
+    animation-name: ${(props) => props.animation};
+    animation-duration: .3s;
+    animation-fill-mode: forwards;
+
+    @keyframes appearRight {
+      0% {
+        transform: translateX(-100%);
+      }
+      100% {
+        transform: translateX(0%);
+      }
+    }
+
+    @keyframes appearLeft {
+       0% {
+        transform: translateX(100%);
+      }
+      100% {
+        transform: translateX(0);
+      }
+    }
+  }
 `
 
 const HeaderInner = styled.section`
@@ -17,15 +65,6 @@ const HeaderInner = styled.section`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-`
-
-const NavItem = styled.div`
-  width: 50%;
-  min-width: 160px;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `
 
 const HeaderTitle = styled.div`
@@ -41,21 +80,53 @@ const HeaderTitle = styled.div`
 `
 
 const Header = () => {
-  const headerTitle = useGetTitle();
+  const [pageState, setPageState] = useState<boolean | null>(true);
+  const [pageTitle, setPageTitle] = useState<string>('');
+  const router = useRouter();
+  const params = useSearchParams();
+  const state =  params.get('state')
+  const pathname = usePathname();
+  
+  useEffect(() => {
+    setPageState(state === "application" ? true : false);
+    
+    if (pathname === "/challenge") {
+      setPageTitle(state === "application" ? "Supersquad" : "Ongoing Challenges");
+    } else if (pathname === "/challenge/my") {
+      setPageTitle("My Challenges")
+    }
+
+  }, [state, pathname])
+
+
   return (
     <HeaderContainer>
       <HeaderInner>
         <HeaderTitle>
-          {headerTitle} 
+          {pageTitle}
         </HeaderTitle>
         <AuthSet />
       </HeaderInner>
       <HeaderInner>
-        <NavItem>
-          onApplication
+        <NavItem
+          onClick={() => {
+            const query = "application"
+            router.push(`${pathname}?state=${query}`)
+          }}
+          display={!pageState ? "none" : "block"}
+          animation={'appearLeft'}
+        >
+          On Application
         </NavItem>
-        <NavItem>
-          onGoing 
+        <NavItem
+          onClick={() => {
+            const query = "going"
+            router.push(`${pathname}?state=${query}`)
+          }}
+          display={!pageState ? "block" : "none"}
+          animation={'appearRight'}
+        >
+          On Going 
         </NavItem>
       </HeaderInner>
     </HeaderContainer>
