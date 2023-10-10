6aef9d695d937aec3496a55d6a4aae7db1597f23
Resister-boy <hassanpumped17@gmail.com>
2023-10-10 12:56:33 +0900

    feat: set AuthSet components

---
+++ src/components/common/authSet/AuthSet.tsx
@@ -0,0 +1,30 @@
+import Profile from '@/components/base/Profile/Profile'
+import { AuthContext } from '@/context/auth'
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+
+const AuthSetContainer = styled.div`
+  width: auto;
+  height: 100%;
+  padding-top: 5px;
+  padding-right: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const AuthSet = () => {
+  const { isLoggined } = useContext(AuthContext);
+
+  return (
+    <AuthSetContainer>
+      {isLoggined ? (
+        <Profile />
+      ) : (
+        <div>로그인</div>
+      )}
+    </AuthSetContainer>
+  )
+}
+
+export default AuthSet
