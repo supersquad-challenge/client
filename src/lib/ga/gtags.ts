965076dc479571bdc6c3ca038cfc3b80e13f90f7
Resister-boy <hassanpumped17@gmail.com>
2023-10-26 14:55:22 +0900

    fix: impl ga stream id

--- src/lib/ga/gtags.ts
+++ src/lib/ga/gtags.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = 'G-060KC2Z1WH';
+export const GA_TRACKING_ID = 'G-TR65WBQ76L';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
